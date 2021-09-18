f11bd209b934b0ce8e4e022880469f5f576d9765
Bert Vandewalle <bert.vandewalle@student.kuleuven.be>
2021-09-18 16:55:36 +0200

    add test

--- test.py
+++ test.py
@@ -9,20 +9,11 @@ from qt_core import *
 
 
 class testWidget(QWidget):
-    def __init__(self,dti):
+    def __init__(self):
         super().__init__()
-        self.dti = dti
-        #self.anotherwidget = QWidget(self)
-        #self.anotherwidget.setMinimumSize(500,500)
-        # self.goal = TimeLineGoal(30,self.dti.players[0],"red",parent=self.anotherwidget)
-        # self.goal.move(50,100)
-        
-        # self.addGoal(self)
-
-
-    def addGoal(self,parent=None):
-        self.goal = TimeLineGoal(30,self.dti.players[0],"red")
-        self.goal.move(50,100)
+        self._layout = QHBoxLayout(self)
+        self.label_test=QLabel("test")
+        self._layout.addWidget(self.label_test)
 
 # APPLICATION TO TEST WIDGET
 # ///////////////////////////////////////////////////////////////
@@ -30,38 +21,11 @@ class _MainApp(QApplication):
     dti = DataInitializer
     def __init__(self, argv):
         super().__init__(argv)
-        dtc = DataController()
-        settings = Settings()
-        self.settings = settings.items
-        self.dti = DataInitializer(dtc,self.settings,self.initUI)
-        
+        self.initUI()
 
     def initUI(self):
-        self.gc = GameController(self.dti)
-        self.gc.game.players = self.dti.players[0:3]
-        self.gc.reset()
-        self.gc.start()
-        self.widget= testWidget(self.dti)       
-        #self.widget = TimeLine(self.gc)
-        #self.widget = QWidget()
-        self.widget.show()
-        self.widget.addGoal(parent=self.widget)
-        # self.widget.addGoal(self.widget.anotherwidget)
-        #self.widget.addGoal(self.gc.game.players[0],"red",10)
-        #self.goal = TimeLineGoal(20,self.gc.game.players[0],team="red",parent=self.widget)
-        # self.goal.setFixedSize(100,60)
-        # self.goal.move(50,0)
-        # self.goal.repaint()
-        # time.sleep(1)
-        # self.goal.move(50,100)
-        # self.goal.repaint()
-        # time.sleep(1)
-        # self.goal.move(50,200)
-        # self.goal.repaint()
-        # time.sleep(1)
-        # self.goal.move(50,300)
-
-
+        self.widget= testWidget()
+        self.widget.show()       
 
 if __name__ == '__main__':
     app = _MainApp(sys.argv)
